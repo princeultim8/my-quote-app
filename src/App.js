50a7a409aba568bc1b1c2528c28410fcdefcc038
Moses <PRiMA@Mosess-MacBook-Pro.local>
2023-03-16 16:47:14 +0000

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BarLoader } from "react-spinners";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchQuote() {
+      const response = await axios.get("https://api.quotable.io/random");
+      const data = response.data;
+      setQuote(data.content);
+      setAuthor(data.author);
+      setLoading(false);
+    }
+    fetchQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <div className="loader">
+          <BarLoader color={"#36D7B7"} />
+        </div>
+      ) : (
+        <div className="quote-container">
+          <h1 className="quote">{quote}</h1>
+          <h2 className="author">- {author}</h2>
+          <button onClick={() => setLoading(true)}>Get Quote</button>
+        </div>
+      )}
     </div>
   );
 }
